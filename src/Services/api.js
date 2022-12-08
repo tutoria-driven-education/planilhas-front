7369189be250e4ace851808fef3a25ddce412eef
vinicius <vinigugelmin@hotmail.com>
2022-12-08 16:04:07 -0300

    fix: https error fix with requesting to specific port

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  // TODO: change later
+  baseURL: process.env.REACT_APP_API_BASE_URL.replace("/api", ":5003"),
 });
 
 export default instance;
