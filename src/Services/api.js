b539b0bb9ee3f7b1efe3753da972ec4e3c9491af
vinicius <vinigugelmin@hotmail.com>
2022-12-08 16:07:38 -0300

    fix: https error fix with requesting to specific port

--- src/Services/api.js
+++ src/Services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // TODO: change later
-  baseURL: process.env.REACT_APP_API_BASE_URL.replace("/api", ":5003"),
+  baseURL: "http://discord-bot.sistemas.driven.com.br:5003", //process.env.REACT_APP_API_BASE_URL,
 });
 
 export default instance;
