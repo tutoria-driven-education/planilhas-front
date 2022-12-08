6f2aa4b78ae60677d2567a0bf226272408bebe82
vinicius <vinigugelmin@hotmail.com>
2022-12-08 16:20:21 -0300

    fix: revert last commits of fix

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // TODO: change later
-  baseURL: "http://discord-bot.sistemas.driven.com.br:5003", //process.env.REACT_APP_API_BASE_URL,
+  baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
 export default instance;
